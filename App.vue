f949aa90b8df65d29160cc3268bf1e38b6e9ab47
lizz770 <91313764+lizz770@users.noreply.github.com>
2022-04-16 11:30:45 +1000

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <Components__table />
+  </div>
+</template>
+
+<script>
+import Components__table from './components/Components__table.vue'
+export default {
+  name:"App",
+  components: {
+    Components__table,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
